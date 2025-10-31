73c7f956c173bc8b3a9fadda48d38c0754e47859
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-10-30 20:42:24 -0400

    initialized Post component and its css stylesheet

---
+++ src/components/Post.jsx
@@ -0,0 +1,12 @@
+import './Post.css';
+
+function Post() {
+    return (
+    <div class = "post">
+        <h2 class="title">title</h2>
+        <p class="body">body message</p>
+    </div>
+    );
+}
+
+export default Post;
