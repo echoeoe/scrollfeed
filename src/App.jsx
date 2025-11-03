b5a7b000d9c8d5c41ff30a2538b26c7671dcbad8
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-11-03 17:51:39 -0500

    moved homepage contents from main app file to home page file

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import Post from './components/Post'; 
+import Home from './pages/Home';
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <Post title="title text" body="body message"/>
-      <button id="loginBtn">...</button>
+    <Home />
     </>
   )
 }
