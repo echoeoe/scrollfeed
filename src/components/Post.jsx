cf807ec4a45ef5297648952625662c80345490b5
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-11-11 00:39:58 -0500

    added environment variables for supabase url and anon key

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -5,6 +5,7 @@ function Post({title, body}) {
     <div className = "post">
         <h2 className ="title">{title}</h2>
         <p className ="body">{body}</p>
+
     </div>
     );
 }
--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,11 @@
 import { createClient } from '@supabase/supabase-js';
 
 //supabase url and anon key 
-const supabaseUrl = 'https://kbdknbqxoxsglqhcxbut.supabase.co';
-const supabaseKey = 'placeholder';
+const supabaseUrl = meta.env.VITE_SUPABASEURL;
+const supabaseKey = meta.env.VITE_SUPABASEKEY;
 
 //single reusable client 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+    supabaseUrl, 
+    supabaseKey
+);
