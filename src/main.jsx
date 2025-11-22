3dc2e81d35bb20d297af05bf1b05ad2dfad1360c
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-11-21 21:30:20 -0500

    routed urls to page components for Home, Login, Editor

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import {App} from './App.jsx'
+import { BrowserRouter, Routes, Route } from "react-router";
+import {Home} from './pages/Home.jsx'
+import {Login} from './pages/Login.jsx'
+import {Editor} from './pages/Editor.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path = "login" element={<Login />} />
+        <Route path = "editor" element={<Editor />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
