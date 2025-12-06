e82de390db17ce6ba0395f4e826b6a9e589a7a71
echoeoe <61526855+echoeoe@users.noreply.github.com>
2025-12-06 02:20:54 -0500

    renamed environment variables for clarity

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 //supabase url and anon key 
-const supabaseUrl = meta.env.VITE_SUPABASEURL;
-const supabaseKey = meta.env.VITE_SUPABASEKEY;
+const supabaseUrl = meta.env.VITE_SUPABASE_URL;
+const supabaseKey = meta.env.VITE_SUPABASE_ANON_KEY;
 
 //single reusable client 
 export const supabase = createClient(
