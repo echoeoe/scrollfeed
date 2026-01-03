a810d93e31349558cf6e5d972fc9bc6009e034eb
echoeoe <61526855+echoeoe@users.noreply.github.com>
2026-01-03 17:08:23 -0500

    Change signup page password fields type to password. Require retyped password to match. Remove password complexity rules, enforce 12 char length.

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -15,8 +15,12 @@ export function Signup(){
             setFormError("Username and password must not be blank.");
             return;
         }
-        else if (password.length < 12 || !/[A-Z]/.test(password) || !/[a-z]/.test(password) ||!/[^\w\s]/.test(password)){
-            setFormError("Password must be at least 12 characters in length and contain at least one uppercase, lowercase, and special character.");
+        else if (password.length < 12){
+            setFormError("Password must be at least 12 characters in length.");
+            return;
+        }
+        else if(password != passwordConfirm){
+            setFormError("Passwords must match.");
             return;
         }
     };
@@ -24,8 +28,8 @@ export function Signup(){
     return(<>
     <h1>Signup</h1>
     <input type="text" placeholder="email" value={email} onChange={(e) => setEmail(e.target.value)}></input><br/><br/>
-    <input type="text" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}></input><br/><br/>
-    <input type="text" placeholder="confirm password" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}></input><br/><br/>
+    <input type="password" placeholder="password" value={password} onChange={(e) => setPassword(e.target.value)}></input><br/><br/>
+    <input type="password" placeholder="confirm password" value={passwordConfirm} onChange={(e) => setPasswordConfirm(e.target.value)}></input><br/><br/>
     <button onClick={()=>handleSignUp()}>sign up</button>
     <p className="formError">{formError}</p>
     </>);
